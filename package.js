183a43ade83dd11fcfc5e0cbb9314c72b66c176f
Wenmoux <45694869+Wenmoux@users.noreply.github.com>
2020-06-13 10:31:53 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,62 @@
+{
+
+  "name": "每日一图",
+
+  "displayName": "daily zuimeia wallpaper",
+
+  "version": "1.0.0",
+
+  "main": "main.js",
+
+  "license": "UNLICENSED",
+
+  "author": {
+
+    "name": "chegege"
+
+  },
+
+  "engineStrick": true,
+
+  "repository": {
+
+    "type": "git",
+
+    "url": ""
+
+  },
+
+  "updates": "",
+
+  "contributes": {
+
+    "search": null
+
+  },
+
+  "homepage": "",
+
+  "engines": {
+
+    "dora": ">=1.9.0"
+
+  },
+
+  "keywords": [
+
+    "Dora.js"
+
+  ],
+
+  "dependencies": {
+
+    "axios": "^0.19.2",
+
+    "fs": "^0.0.1-security",
+
+    "http": "^0.0.1-security"
+
+  }
+
+}
+
